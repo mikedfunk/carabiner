dfe2f6e61aac95cde67701bbe1c4a70fba6c9d90
Mike Funk <mfunk@xulonpress.com>
2012-03-04 11:56:27 -0500

    added autoload, commented out.

---
+++ config/autoload.php
@@ -0,0 +1,27 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * autoload
+ * 
+ * Autoloads CI Authentication assets
+ * 
+ * @license		http://www.apache.org/licenses/LICENSE-2.0  Apache License 2.0
+ * @author		Mike Funk
+ * @link		http://mikefunk.com
+ * @email		mike@mikefunk.com
+ * 
+ * @file		autoload.php
+ * @version		1.0
+ * @date		03/04/2012
+ * 
+ * Copyright (c) 2012
+ */
+
+// --------------------------------------------------------------------------
+
+// $autoload['library'] = array('carabiner');
+// $autoload['config'] = array('carabiner');
+
+// --------------------------------------------------------------------------
+
+/* End of file autoload.php */
+/* Location: ./carabiner/config/autoload.php */
